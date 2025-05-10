1791d5cb32e73f4315612fc2592636cf3b26f12f
WipeOutJunk <imperia791@gmail.com>
2025-05-10 15:17:15 +0700

    swagger i

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('AI Testing Platform API')
+    .setDescription('Документация REST API для дипломного проекта')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // будет доступно на /api
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
